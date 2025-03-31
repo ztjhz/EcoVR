5dc271d805587f4c1440cee901195d5d5a9ed1d4
Noel Wee <104272626+NoelWee@users.noreply.github.com>
2025-04-01 00:18:28 +0800

    hunger and thirst mechanic (#26)
    * hunger and thirst mechanic
    
    - rescaled animals and colliders
    - implemented increase fullness in accordance to feeding
    
    * revert mat_cloudSphere.mat
    
    * revert font
    
    ---------
    
    Co-authored-by: Jing Hua <tohjinghua123@gmail.com>

--- Assets/SCRIPTS/IAnimalStatus.cs
+++ Assets/SCRIPTS/IAnimalStatus.cs
@@ -3,6 +3,7 @@ public interface IAnimalStatus
     void DecreaseHydration();
     void IncreaseHydration();
     void DecreaseFullness();
+    void IncreaseFullness(); 
     void ModifyHuntingRadius(float multiplier);
     bool IsPredator();
 }
