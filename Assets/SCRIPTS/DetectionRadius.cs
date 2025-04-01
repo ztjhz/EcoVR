7cf28db6459fa0c824e01864bbbfea2364b4d78d
Jing Hua <tohjinghua123@gmail.com>
2025-04-01 22:22:57 +0800

    add toggling to radius

--- Assets/SCRIPTS/DetectionRadius.cs
+++ Assets/SCRIPTS/DetectionRadius.cs
@@ -6,12 +6,15 @@ public class DetectionRadius : MonoBehaviour
     private AnimationController preyAI;
     private PredatorAI predatorAI;
     private float radius;
-    private bool isDetectionActive = false;
+    private InfoToggleManager infoToggleManager;
+    private bool isDetectionActive;
 
     private void Start()
     {
         predatorAI = GetComponentInParent<PredatorAI>();
         preyAI = GetComponentInParent<AnimationController>();
+        infoToggleManager = GameObject.Find("InfoToggleManager").GetComponent<InfoToggleManager>();
+        isDetectionActive = infoToggleManager.isInfoActive;
 
         if (predatorAI != null)
             radius = predatorAI.detectionRange;
@@ -19,17 +22,24 @@ public class DetectionRadius : MonoBehaviour
         if (preyAI != null)
             radius = preyAI.detectionRange;
 
-        SetRadius(radius);
-    }
-
-    public void ToggleDetectionRadius()
-    {
-        isDetectionActive = !isDetectionActive;
-
         if (isDetectionActive)
             SetRadius(radius);
         else
             SetRadius(0);
+    }
+
+    private void Update()
+    {
+        if (isDetectionActive != infoToggleManager.isInfoActive)
+        {
+            isDetectionActive = infoToggleManager.isInfoActive;
+
+            if (isDetectionActive)
+                SetRadius(radius);
+            else
+                SetRadius(0);
+        }
+
     }
 
     private void SetRadius(float radius)
